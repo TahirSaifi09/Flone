15cbc9ddcb499677b8deb11a06b3e002c07d8c8a
tahirsaifi09 <tahirsaifi413@gmail.com>
2025-04-03 19:52:21 +0530

    Add logo and adjusted padding in footer

--- flone/src/component/Footer.jsx
+++ flone/src/component/Footer.jsx
@@ -1,6 +1,11 @@
 export default function Footer(){
     return(
-        <div className="w-full flex justify-between px-44 py-20 bg-gray-100">
+        <div className="w-full flex justify-between items-center px-44 py-20 bg-gray-100">
+            <div className="flex flex-col gap-3 text-gray-500">
+                <img src="logo.png" alt="logo" />
+                <p>© 2025 Flone. </p>
+                <p>All Rights Reserved</p>
+            </div>
             <ul className="flex flex-col gap-3 text-gray-500">
                 <li className="text-black">ABOUT US</li>
                 <li>About us</li>
@@ -22,10 +27,13 @@ export default function Footer(){
                 <li>Instagram</li>
                 <li>Youtube</li>
             </ul>
-            <div className="flex flex-col gap-3 text-gray-500">
+            <div className="flex flex-col gap-3  text-gray-500 w-1/4">
                 <p className="text-black">SUBSCRIBE</p>
                 <p>Get E-mail updates about our latest shop and special offers.</p>
                 <input type="email" name="email" placeholder="Enter your email address..." className="outline-none border-b py-2" />
+                <div>
+                <button className="border-b hover:text-violet-600 duration-400 text-black cursor-pointer">SUBSCRIBE</button>
+                </div>
             </div>
         </div>
     )
